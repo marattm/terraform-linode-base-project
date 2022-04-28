fdec82905106ab6c3863d23697ccc2eacb627b98
Marat Monnie <marat.monnie@gmail.com>
2022-04-27 22:14:05 -0500

    - updated variables - added providers.tf - added backend.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,8 @@
-terraform {
-  required_providers {
-    linode = {
-      source  = "linode/linode"
-      version = "1.26.1"
-    }
-  }
-
-  /*
-  // https://adriano.fyi/post/2020-05-29-how-to-use-linode-object-storage-as-a-terraform-backend/ 
-
-  backend "s3" {
-    bucket = "test-bucket-jesh"
-    key    = "state.tfstate"
-    region = "ap-south-1"                      # e.g. us-east-1
-    endpoint = "ap-south-1.linodeobjects.com"    # e.g. us-est-1.linodeobjects.com
-    skip_credentials_validation = true                # just do it
-    access_key = ""
-    secret_key = ""
-  }
-  */
-}
-
-provider "linode" {
-  token = var.api_token
-}
-
+# module invocations
 module "nodebalancer" {
   # source                  = "./modules/nodebalancer"
   source                  = "git@github.com:marattm/terraform-module-linode-nodebalancer.git"
-  region                  = var.region
+  region                  = var.LN_REGION
   node_count              = var.node_count
   web_servers_private_ips = module.webserver.web_servers_private_ips
 }
@@ -38,7 +12,7 @@ module "webserver" {
   source              = "git@github.com:marattm/terraform-module-linode-webserver.git"
   public_key_location = var.public_key_location
   root_password       = var.root_password
-  region              = var.region
+  region              = var.LN_REGION
   node_count          = var.node_count
   instance_type       = var.instance_type
 }
@@ -48,7 +22,7 @@ module "dbserver" {
   source              = "git@github.com:marattm/terraform-module-linode-dbserver.git"
   public_key_location = var.public_key_location
   root_password       = var.root_password
-  region              = var.region
+  region              = var.LN_REGION
   db_node_count       = var.db_node_count
   db_instance_type    = var.db_instance_type
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,22 @@
-variable "api_token" {}
-variable "region" {}
+variable "LN_API_TOKEN" {}
+variable "LN_REGION" {}
+
+
+variable "authorized_keys" {}
 variable "root_password" {}
 variable "public_key_location" {}
 variable "node_count" {}
 variable "instance_type" {}
 variable "db_node_count" {}
 variable "db_instance_type" {}
+
 # variable "authorized_keys" {
 #   default = ""
 #   type    = list(string)
 # }
+
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {}
+variable "AWS_S3_BACKEND_BUCKET" {}
+variable "AWS_S3_BACKEND_BUCKET_KEY" {}
