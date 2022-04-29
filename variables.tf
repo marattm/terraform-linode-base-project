eb7e725cd2d7b49d8362cd6d9b265d69a71570bc
Marat Monnie <marat.monnie@gmail.com>
2022-04-29 16:03:00 -0500

    - removed unused aws bucket variables

--- variables.tf
+++ variables.tf
@@ -20,8 +20,6 @@ variable "LN_REGION" {
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {}
-variable "AWS_S3_BACKEND_BUCKET" {}
-variable "AWS_S3_BACKEND_BUCKET_KEY" {}
 
 
 # variable "authorized_keys" {
