5c117ef6817accdfede494fadc83f90b9a794f43
Marat Monnie <marat.monnie@gmail.com>
2022-05-04 12:00:30 -0500

    - added private ip to outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,16 @@ output "db_servers_public_ips" {
 output "web_servers_public_ips" {
   value = module.webserver.web_servers_public_ips
 }
+
+
+output "db_servers_private_ips" {
+  value = module.dbserver.db_servers_private_ips
+}
+output "web_servers_private_ips" {
+  value = module.webserver.web_servers_private_ips
+}
+
+# output "web_servers_passwd" {
+#   value = module.webserver.web_servers_passwd
+# }
+
